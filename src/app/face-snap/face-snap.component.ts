231e3f90d21e488679d1b9b3acacaf0166d028f9
LBF38 <contact.lbf38@gmail.com>
2022-12-04 14:46:56 +0100

    Attribute binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,11 +10,13 @@ export class FaceSnapComponent implements OnInit {
   description!: string;
   createdDate!: Date;
   snaps!: number;
+  imageUrl!: string;
 
   ngOnInit() {
     this.title = 'FaceSnap';
     this.description = 'A social media app for sharing photos';
     this.createdDate = new Date();
     this.snaps = 0;
+    this.imageUrl = 'https://www.w3schools.com/w3images/avatar2.png';
   }
 }
