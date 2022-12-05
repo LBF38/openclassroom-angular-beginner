91615bd3c712432d7b01f64eee7e6035126e4aac
LBF38 <contact.lbf38@gmail.com>
2022-12-05 18:20:41 +0100

    Discovering ngFor to add more dynamic data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,37 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  faceSnaps!: FaceSnap[];
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit() {
-    this.mySnap = {
-      title: 'FaceSnap',
-      description: 'A social media app for sharing photos',
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'https://www.w3schools.com/w3images/avatar2.png',
-      location: 'New York'
-    };
-    this.myOtherSnap = {
-      title: 'FaceSnap',
-      description: "This is a second FaceSnap",
-      createdDate: new Date(),
-      snaps: 0,
-      imageUrl: 'https://picsum.photos/200/300',
-      location: "La montagne"
-    };
-    this.myLastSnap = {
-      title: 'FaceSnap',
-      description: "Another FaceSnap",
-      createdDate: new Date(),
-      snaps: 5,
-      imageUrl: 'https://picsum.photos/200/300?grayscale'
-    };
+    this.faceSnaps = [
+      {
+        title: 'FaceSnap',
+        description: 'A social media app for sharing photos',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://www.w3schools.com/w3images/avatar2.png',
+        location: 'New York'
+      },
+      {
+        title: 'FaceSnap',
+        description: "This is a second FaceSnap",
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://picsum.photos/200/300',
+        location: "La montagne"
+      },
+      {
+        title: 'FaceSnap',
+        description: "Another FaceSnap",
+        createdDate: new Date(),
+        snaps: 5,
+        imageUrl: 'https://picsum.photos/200/300?grayscale'
+      },
+    ];
   }
 
 }
